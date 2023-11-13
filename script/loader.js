e195a461011700d3ef5cd163804736ebe6310a51
ben <150052679+benie1242hs@users.noreply.github.com>
2023-11-13 08:09:03 +0000

    Add files via upload

---
+++ script/loader.js
@@ -0,0 +1,9 @@
+<Script>
+var loader = document.querySelector(".loader")
+window.addEventListener("load", vanish);
+
+function vanish() {
+loader.classList.add("disppear");
+
+}
+</Script>
---
+++ script/side-bar.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+$(document).ready(function(){
+  $('.sub-btn').click(function(){
+
+$(this).next('.sub-menu').slideTongle();
+  })
+});
+</script>
